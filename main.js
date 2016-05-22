e212fc33a7ff793ba6960a8c2f333b2f8ff23f84
Rishabh <rishabh.pugalia@gmail.com>
2016-05-23 01:05:05 +0530

    https for github pages

--- main.js
+++ main.js
@@ -14,7 +14,8 @@
     // of different video formats to add
     src: [
       {
-        src: 'night.mp4',
+        // src: 'night.mp4',
+        src: 'http://rishabhp.github.io/bideo.js/night.mp4',
         type: 'video/mp4'
       }
     ],
