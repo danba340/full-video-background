95fa203bbd4d495cb98b9b3b51413e4231cf5724
danba340 <danba340@gmail.com>
2020-08-11 18:41:38 +0200

    initial

--- main.js
+++ main.js
@@ -1,37 +1,20 @@
 (function () {
-
-  var bv = new Bideo();
-  bv.init({
-    // Video element
+  const bideo = new Bideo();
+  bideo.init({
     videoEl: document.querySelector('#background_video'),
-
-    // Container element
     container: document.querySelector('body'),
-
-    // Resize
     resize: true,
-
-    // autoplay: false,
-
+    autoplay: true, // true is default
     isMobile: window.matchMedia('(max-width: 768px)').matches,
-
     playButton: document.querySelector('#play'),
     pauseButton: document.querySelector('#pause'),
-
-    // Array of objects containing the src and type
-    // of different video formats to add
     src: [
       {
-        src: 'night.mp4',
+        src: 'earth.mp4',
         type: 'video/mp4'
-      },
-      {
-        src: 'night.webm',
-        type: 'video/webm;codecs="vp8, vorbis"'
       }
     ],
-
-    // What to do once video loads (initial frame)
+    // On video loaded
     onLoad: function () {
       document.querySelector('#video_cover').style.display = 'none';
     }
