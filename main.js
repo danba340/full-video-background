a75ad40d824bb5c2818792dac077b323a365bc35
danba340 <danba340@gmail.com>
2020-08-12 19:17:45 +0200

    Finished

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 (function () {
   const bideo = new Bideo();
   bideo.init({
-    videoEl: document.querySelector('#background_video'),
-    container: document.querySelector('body'),
+    videoEl: document.querySelector("#background_video"),
+    container: document.querySelector("body"),
     resize: true,
     autoplay: true, // true is default
-    isMobile: window.matchMedia('(max-width: 768px)').matches,
-    playButton: document.querySelector('#play'),
-    pauseButton: document.querySelector('#pause'),
+    isMobile: window.matchMedia("(max-width: 768px)").matches,
+    playButton: document.querySelector("#play"),
+    pauseButton: document.querySelector("#pause"),
     src: [
       {
-        src: 'earth.mp4',
-        type: 'video/mp4'
-      }
+        src: "earth.mp4",
+        type: "video/mp4",
+      },
     ],
     // On video loaded
     onLoad: function () {
-      document.querySelector('#video_cover').style.display = 'none';
-    }
+      // document.querySelector('#video_cover').style.display = 'none';
+    },
   });
 }());
